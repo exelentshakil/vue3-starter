475bc0e565fbbef881cf45ffca3cfd895dc7d7c9
Shakil Ahmed <exelentshakil@gmail.com>
2021-05-07 07:27:19 +0600

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <todos title="Shak's amazing todo list"></todos>
+</template>
+
+<script>
+import Todos from './components/todos'
+
+export default {
+  name: 'App',
+  components: {
+    Todos
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
